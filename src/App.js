38f5b2eeb93aa10defc0fdd30d9f93e3e890fc8a
Shenyu Li <shenyuli@Shenyus-MacBook-Air.local>
2023-08-01 12:57:39 -0700

    clean code and add stylings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Routes, Route, useParams, Link, Outlet } from 'react-router-dom';
-//import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Index';
 import Articles from './pages/articles/Articles';
 import About from './pages/about/About';
 import NotFound from './pages/NotFound';
 import Layouts from './views/layout';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 
 function App() {
   return (
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,39 +1,28 @@
 import React from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import ArticleIcon from '@mui/icons-material/Article';
 import BadgeIcon from '@mui/icons-material/Badge';
 import { NavLink } from 'react-router-dom';
 
 export default () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   const linkActive = ({isActive}) => {
     return {
       fontWeight: 'bold',
       color: isActive ? 'blue' : 'inherit',
       textDecoration: 'none',
       margin: '5px',
+      display: 'flex',
+      alignItems: 'center',
+      width: '100px'
     }
   }
 
   return (
     <Box sx={{ maxWidth: { xs: 320, sm: 480, md: '100%' } }} className='nav'>
-      <NavLink to='/' style={linkActive}>Home</NavLink>
-      <NavLink to='/articles' style={linkActive}>Articles</NavLink>
-      <NavLink to='/about' style={linkActive}>About</NavLink>
-      {/* <Tabs
-        value={value}
-        onChange={handleChange}
-        centered
-      >
-        <Tab icon={<HomeIcon />} iconPosition="start" label="Home" href="/" />
-        <Tab icon={<ArticleIcon />} iconPosition="start" label="Articles" href="/articles" />
-        <Tab icon={<BadgeIcon />} iconPosition="start" label="About" href="/about" />
-      </Tabs> */}
+      <NavLink to='/' style={linkActive}><HomeIcon /> Home</NavLink>
+      <NavLink to='/articles' style={linkActive}><ArticleIcon /> Articles</NavLink>
+      <NavLink to='/about' style={linkActive}><BadgeIcon /> About</NavLink>
     </Box>
   );
 }
