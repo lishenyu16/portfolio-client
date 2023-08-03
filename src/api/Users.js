cff8cb54c8268e5461d976a17587348fc8d5fcc9
Shenyu Li <shenyuli@Shenyus-MacBook-Air.local>
2023-08-02 17:57:56 -0700

    fix url

--- src/api/Users.js
+++ src/api/Users.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 const baseUrl = process.env.NODE_ENV === 'production' ? 'shenyu16.com' : 'http://localhost:3003';
 export const fetchUsers = () => {
-  return axios(baseUrl + `/api/auth/users`);
+  return axios(`/api/auth/users`);
 }
 
 export const createUser = (user) => {
-  return axios.post(baseUrl + '/api/auth/addUser', {user});
+  return axios.post('/api/auth/addUser', {user});
 }
